c0a48e914303c802a705639e395ecdc94ab0eca6
int2001 <Joerg int2001@users.noreply.github.com>
2023-07-17 14:13:45 +0200

    Initial Commit

---
+++ charts.js
@@ -0,0 +1,69 @@
+$(function() {
+	var dxcluster_provider = 'https://dxc.jo30.de/dxcache';
+
+	function render_chart (band,spot_data) {
+
+		Highcharts.chart('container', {
+			chart: {
+				type: 'timeline'
+			},
+			accessibility: {
+				screenReaderSection: {
+					beforeChartFormat: '<h5>{chartTitle}</h5>' +
+						'<div>{typeDescription}</div>' +
+						'<div>{chartSubtitle}</div>' +
+						'<div>{chartLongdesc}</div>' +
+						'<div>{viewTableButton}</div>'
+				},
+				point: {
+					valueDescriptionFormat: '{index}. {point.label}. {point.description}.'
+				}
+			},
+			xAxis: {
+				visible: true,
+				type: 'linear'
+			},
+			yAxis: {
+				visible: false
+			},
+			title: {
+				text: band
+			},
+			series: [ { data: spot_data } ] 
+		});
+	}
+
+	function SortByQrg(a, b){
+		var a = a.frequency;
+		var b = b.frequency;
+		return ((a< b) ? -1 : ((a> b) ? 1 : 0));
+	}
+
+	function convert2high(spotobject) {
+		let ret={};
+		ret.name=spotobject.spotted;
+		ret.x=spotobject.frequency;
+		ret.description=spotobject.frequency;
+		return ret;
+	}
+
+	function set_chart(lowerQrg,upperQrg,maxAgeMinutes) {
+		$.ajax({
+			url: dxcluster_provider + "/spots",
+			cache: false,
+			dataType: "json"
+		}).done(function(dxspots) {
+			spots4chart=[];
+			dxspots.sort(SortByQrg);
+			dxspots.forEach((single) => {
+				if ( (single.frequency >= lowerQrg) && (single.frequency <= upperQrg) && (Date.parse(single.when)>(Date.now() - 1000 * 60 * maxAgeMinutes)) ) {
+					spots4chart.push(convert2high(single));
+				}
+			});
+			console.log(spots4chart);
+			render_chart('20m',spots4chart);
+		});
+	}
+
+set_chart(14000,14310,30);
+});
